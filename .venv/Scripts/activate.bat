cf53578539961663eb103a076080f58bbbe51096
Ben Alderson <benalderson725@gmail.com>
2021-10-21 13:43:58 -0400

    V2 - Saves to Excel and uses threads

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\benal\OneDrive\Archive\Documents\Python\Minecraft Server Scanner\.venv
+set VIRTUAL_ENV=C:\Users\benal\OneDrive\Archive\Documents\Python\Ngrok Minecraft Server Finder\Ngrok-Minecraft-Server-Finder\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
